976087c14b347069782d28acc67f6b9baeb6eddd
Paulo Orquillo <olopsman@users.noreply.github.com>
2022-02-26 12:17:16 +1300

    Initial Commit

---
+++ SwifftUICharts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwifftUICharts
+//
+//  Created by Paulo Orquillo on 26/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwifftUICharts/SwifftUIChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifftUIChartsApp.swift
+//  SwifftUICharts
+//
+//  Created by Paulo Orquillo on 26/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SwifftUIChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

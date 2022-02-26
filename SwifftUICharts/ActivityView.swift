fa964bdd70981741d5f49b9276ad4fd4e4d1843a
Paulo Orquillo <olopsman@users.noreply.github.com>
2022-02-26 22:26:27 +1300

    bar chart sample

--- SwifftUICharts/ActivityView.swift
+++ SwifftUICharts/ActivityView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 struct ActivityView: View {
     @State private var moveValues: [Double] = ActivityView.mockData(24, in: 0...300)
+    @State private var exerciseValues: [Double] = ActivityView.mockData(24, in: 0...60)
+    @State private var standValues: [Double] = ActivityView.mockData(24, in: 0...1)
+
     var body: some View {
         VStack(alignment: .leading) {
             
             Text("Move").bold()
                 .foregroundColor(.red)
-            
             BarChartView(data: moveValues, colors: [.red, .orange])
+
+            Text("Exercise").bold()
+                .foregroundColor(.green)
+            BarChartView(data: exerciseValues, colors: [.green, .yellow])
+            
+            Text("Stand").bold()
+                .foregroundColor(.blue)
+            BarChartView(data: standValues, colors: [.blue, .purple])
         }
+        .padding()
     }
     
     
